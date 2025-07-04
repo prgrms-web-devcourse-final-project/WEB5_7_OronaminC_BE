601df18c47dc6990ed271241b4fb493a33456023
최희웅 <choehuiung@choehuiung-ui-MacBookPro.local>
2025-07-04 14:35:40 +0900

    init: 프로젝트 초기 세팅

---
+++ src/main/java/com/oronaminc/join/Web57OronaminCBeApplication.java
@@ -0,0 +1,13 @@
+package com.oronaminc.join;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Web57OronaminCBeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Web57OronaminCBeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/oronaminc/join/Web57OronaminCBeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.oronaminc.join;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Web57OronaminCBeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
