3573b0b3fd68bec965fa7277ee7407200a3b3e0d
김건우 <96411818+rjswjddn@users.noreply.github.com>
2025-07-08 15:19:42 +0900

    [Feat]: 회원 API 구현 (#11)
    * chore: gitignore에 yml 추가
    * feat: member controller, service, repository 생성
    * feat: 카카오 로그인 구현
    * feat: 게스트 로그인 구현
    * feat: 로그인 확인 API 구현
    * feat: 로그아웃 API 구현
    * refactor: 리팩토링
    * fix: 게스트 로그인 응답 수정
    * feat: 게스트 닉네임 길이 검증 추가
    * chore: .env 파일 설정

---
+++ src/main/java/com/oronaminc/join/member/security/AuthService.java
@@ -0,0 +1,86 @@
+package com.oronaminc.join.member.security;
+
+import java.util.Map;
+import java.util.Optional;
+
+import org.springframework.security.oauth2.client.userinfo.DefaultOAuth2UserService;
+import org.springframework.security.oauth2.client.userinfo.OAuth2UserRequest;
+import org.springframework.security.oauth2.core.OAuth2AuthenticationException;
+import org.springframework.security.oauth2.core.user.OAuth2User;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.oronaminc.join.member.domain.Member;
+import com.oronaminc.join.member.domain.MemberType;
+import com.oronaminc.join.member.dto.GuestLoginRequest;
+import com.oronaminc.join.member.repository.MemberJpaRepository;
+
+import jakarta.servlet.http.HttpSession;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+
+@Service
+@Slf4j
+@RequiredArgsConstructor
+public class AuthService extends DefaultOAuth2UserService {
+    private final MemberJpaRepository memberJpaRepository;
+
+    private final HttpSession httpSession;
+
+    @Override
+    public OAuth2User loadUser(OAuth2UserRequest userRequest) throws OAuth2AuthenticationException {
+        OAuth2User oAuth2User = super.loadUser(userRequest);
+        Map<String, Object> attributes = oAuth2User.getAttributes();
+
+        log.info("attributes :: " + attributes);
+
+        Map<String, Object> kakaoAccount = (Map<String, Object>) attributes.get("kakao_account");
+        Map<String, Object> profile = (Map<String, Object>) kakaoAccount.get("profile");
+
+
+        Optional<Member> optionalMember = memberJpaRepository.findByEmail(kakaoAccount.get("email").toString());
+
+        Member member = optionalMember.orElseGet(
+                () -> memberJpaRepository.save(
+                        Member.builder()
+                                .email(kakaoAccount.get("email").toString())
+                                .nickname(profile.get("nickname").toString())
+                                .profileImage(profile.get("profile_image_url").toString())
+                                .memberType(MemberType.MEMBER)
+                                .build()
+                )
+        );
+
+        return MemberDetails.builder()
+                .id(member.getId())
+                .name(member.getEmail())
+                .nickname(member.getNickname())
+                .attributes(attributes)
+                .role(member.getMemberType())
+                .build();
+    }
+
+    @Transactional
+    public MemberDetails loadGuest(GuestLoginRequest guestLoginRequest) {
+        Member guest = Member.builder()
+                .email(null)
+                .nickname(guestLoginRequest.nickname())
+                .profileImage(null)
+                .memberType(MemberType.GUEST)
+                .build();
+
+        memberJpaRepository.save(guest);
+
+        // 1. 비회원 MemberDetails 생성
+        MemberDetails memberDetails = MemberDetails.builder()
+                .id(guest.getId())
+                .name("GUEST" + guest.getId())
+                .nickname(guest.getNickname())
+                .role(MemberType.GUEST)
+                .attributes(Map.of("nickname", guest.getNickname()))
+                .build();
+
+        return memberDetails;
+    }
+
+}
---
+++ src/main/java/com/oronaminc/join/member/security/SecurityConfig.java
@@ -0,0 +1,48 @@
+package com.oronaminc.join.member.security;
+
+import static org.springframework.security.config.Customizer.*;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.web.SecurityFilterChain;
+
+import lombok.RequiredArgsConstructor;
+
+@Configuration
+@EnableWebSecurity
+@RequiredArgsConstructor
+public class SecurityConfig {
+    private final AuthService authService;
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        return http
+                .csrf(csrf -> csrf.disable())
+                .authorizeHttpRequests(auth -> auth
+                        .requestMatchers(
+                                "/api/auth/guest",
+                                "/login"
+                        )
+                        .anonymous()
+                        .requestMatchers(
+                                "/swagger-ui/**",
+                                "/swagger-resources/**",
+                                "/v3/api-docs/**",
+                                "/oauth2/authorization/**",
+                                "/login/oauth2/code/kakao",
+                                "/api/auth/logout"
+                        )
+                        .permitAll()
+                        .anyRequest().authenticated()
+                )
+                .formLogin(AbstractHttpConfigurer::disable)
+                .oauth2Login(oauth2 -> oauth2.userInfoEndpoint(userInfo -> userInfo
+                        .userService(authService)))
+                .logout(withDefaults())
+                .build();
+    }
+
+}
