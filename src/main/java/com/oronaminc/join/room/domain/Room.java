6b9d28cc7cf902028608d834936b13b833ca4a42
chaechae <juold0568@naver.com>
2025-07-08 17:11:51 +0900

    feat: entity에 builder 추가

--- src/main/java/com/oronaminc/join/room/domain/Room.java
+++ src/main/java/com/oronaminc/join/room/domain/Room.java
@@ -4,6 +4,8 @@ package com.oronaminc.join.room.domain;
 import com.oronaminc.join.global.entity.BaseEntity;
 import jakarta.persistence.*;
 import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
@@ -11,6 +13,8 @@ import java.time.LocalDateTime;
 
 @Entity
 @Getter
+@Builder
+@AllArgsConstructor
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class Room extends BaseEntity {
 
