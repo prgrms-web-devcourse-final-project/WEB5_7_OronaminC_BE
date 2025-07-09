941223c09901f6aa85c65abfd823f6a2931e4b9b
min0962 <wsx8028@gmail.com>
2025-07-09 10:17:13 +0900

    refactor : valid 추가

--- src/main/java/com/oronaminc/join/member/api/MemberController.java
+++ src/main/java/com/oronaminc/join/member/api/MemberController.java
@@ -6,11 +6,13 @@ import com.oronaminc.join.member.dto.MyProfileUpdateRequest;
 import com.oronaminc.join.member.dto.MyProfileUpdateResponse;
 import com.oronaminc.join.member.dto.MyRoomsGetResponse;
 import com.oronaminc.join.member.service.MyPageService;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Pageable;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PatchMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseStatus;
@@ -33,7 +35,7 @@ public class MemberController {
 
     @PatchMapping("/me")
     @ResponseStatus(HttpStatus.OK)
-    public MyProfileUpdateResponse updateMyProfile(MyProfileUpdateRequest request) {
+    public MyProfileUpdateResponse updateMyProfile(@RequestBody @Valid MyProfileUpdateRequest request) {
         // todo : 세션로그인 도입 후 memberID 세션에서 받아오는 걸로 변경
         Long memberId = 1L;
         return myPageService.updateMyProfile(request, memberId);
