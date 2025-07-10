021a714ce207a7715f14c2c0087dfd39dd969cfd
chaechae <juold0568@naver.com>
2025-07-10 11:43:38 +0900

    feat: dto 설명 추가

--- src/main/java/com/oronaminc/join/question/dto/QuestionListResponse.java
+++ src/main/java/com/oronaminc/join/question/dto/QuestionListResponse.java
@@ -1,7 +1,9 @@
 package com.oronaminc.join.question.dto;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import java.util.List;
 
+@Schema(description = "질문 목록을 묶기 위한 DTO")
 public record QuestionListResponse(
     List<QuestionAssembleResponse> questions
 ) {
