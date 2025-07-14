c2a2ac79f7eb1b82ba6469d807791c75e2cea3bc
min0962 <wsx8028@gmail.com>
2025-07-14 12:41:51 +0900

    feat: 공감 동시성 제어를 위한 EmojiFacade 추가

---
+++ src/main/java/com/oronaminc/join/emoji/service/EmojiFacade.java
@@ -0,0 +1,29 @@
+package com.oronaminc.join.emoji.service;
+
+import com.oronaminc.join.emoji.dto.EmojiRequest;
+import com.oronaminc.join.emoji.dto.EmojiResponse;
+import com.oronaminc.join.global.exception.ErrorCode;
+import com.oronaminc.join.global.exception.ErrorException;
+import lombok.RequiredArgsConstructor;
+import org.springframework.orm.ObjectOptimisticLockingFailureException;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class EmojiFacade {
+
+    private final EmojiService emojiService;
+
+    public EmojiResponse toggleEmoji(Long memberId, EmojiRequest emojiRequest)
+        throws InterruptedException {
+        for (int i = 0; i < 10; i++) {
+            try {
+                return emojiService.toggleEmoji(memberId, emojiRequest);
+            } catch (ObjectOptimisticLockingFailureException e) {
+                Thread.sleep(50);
+            }
+        }
+        throw new ErrorException(ErrorCode.EMOJI_CONFLICT);
+    }
+
+}
