2e24d58560458f42c4f4da36b387f3544881a2f5
김건우 <96411818+rjswjddn@users.noreply.github.com>
2025-07-16 11:46:38 +0900

    feat: 발표방 참여 및 입장 인원 계산 로직 (#57)
    * feat: 방 참가 웹소켓 구현
    * feat: 방 퇴장 웹소켓 구현
    * feat: 방 구독 및 참여 조건 구현
    * feat: 방 상세조회 인원 수정
    * feat: 세션 관리, 방 퇴장 수정

--- src/main/java/com/oronaminc/join/websocket/config/WebSocketConfig.java
+++ src/main/java/com/oronaminc/join/websocket/config/WebSocketConfig.java
@@ -1,14 +1,17 @@
 package com.oronaminc.join.websocket.config;
 
-import lombok.RequiredArgsConstructor;
+import org.springframework.context.ApplicationEventPublisher;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.messaging.simp.config.MessageBrokerRegistry;
 import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
 import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
 import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
+import org.springframework.web.socket.config.annotation.WebSocketTransportRegistration;
 import org.springframework.web.socket.handler.WebSocketHandlerDecoratorFactory;
 import org.springframework.web.socket.server.support.HttpSessionHandshakeInterceptor;
+
+import lombok.RequiredArgsConstructor;
 
 @Configuration
 @RequiredArgsConstructor
@@ -17,11 +20,15 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
 
     private final CustomHandshakeHandler handshakeHandler;
     private final StompErrorHandler stompErrorHandler;
+    private final WebsocketSessionManager sessionManager;
+    private final ApplicationEventPublisher publisher;
 
     @Bean
     public WebSocketHandlerDecoratorFactory webSocketHandlerDecoratorFactory(
-        WebsocketSessionManager sessionManager) {
-        return delegate -> new CustomWebSocketHandlerDecorator(delegate, sessionManager);
+            WebsocketSessionManager sessionManager,
+            ApplicationEventPublisher publisher
+    ) {
+        return delegate -> new CustomWebSocketHandlerDecorator(delegate, sessionManager, publisher);
     }
 
     @Override
@@ -34,18 +41,23 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
     @Override
     public void registerStompEndpoints(StompEndpointRegistry registry) {
         registry.addEndpoint("/ws")
-            .setAllowedOriginPatterns("*")
-            // websocket 연결 전 쿠키 체크
-            .addInterceptors(new HttpSessionHandshakeInterceptor())
-            // websocket 연결 후 principal 생성
-            .setHandshakeHandler(handshakeHandler)
-            .withSockJS();
+                .setAllowedOriginPatterns("*")
+                // websocket 연결 전 쿠키 체크
+                .addInterceptors(new HttpSessionHandshakeInterceptor())
+                // websocket 연결 후 principal 생성
+                .setHandshakeHandler(handshakeHandler)
+                .withSockJS();
 
         registry.addEndpoint("/ws")
-            .setAllowedOriginPatterns("*")
-            .addInterceptors(new HttpSessionHandshakeInterceptor())
-            .setHandshakeHandler(handshakeHandler);
+                .setAllowedOriginPatterns("*")
+                .addInterceptors(new HttpSessionHandshakeInterceptor())
+                .setHandshakeHandler(handshakeHandler);
 
         registry.setErrorHandler(stompErrorHandler);
+    }
+
+    @Override
+    public void configureWebSocketTransport(WebSocketTransportRegistration registry) {
+        registry.setDecoratorFactories(webSocketHandlerDecoratorFactory(sessionManager, publisher));
     }
 }
