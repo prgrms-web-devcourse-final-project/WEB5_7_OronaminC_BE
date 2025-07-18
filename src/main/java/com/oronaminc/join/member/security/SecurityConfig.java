106985df99773e073c1416328a0888182cd5e151
김건우 <rjswjddn@gmail.com>
2025-07-18 15:17:03 +0900

    test: Room 캐시 테스트

--- src/main/java/com/oronaminc/join/member/security/SecurityConfig.java
+++ src/main/java/com/oronaminc/join/member/security/SecurityConfig.java
@@ -4,6 +4,7 @@ import static org.springframework.security.config.Customizer.*;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
@@ -12,6 +13,7 @@ import org.springframework.security.web.SecurityFilterChain;
 import lombok.RequiredArgsConstructor;
 
 @Configuration
+@Profile("!test")
 @EnableWebSecurity
 @RequiredArgsConstructor
 public class SecurityConfig {
