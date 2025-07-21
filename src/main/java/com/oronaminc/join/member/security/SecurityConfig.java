7e498b8d99caaeabbeda613a9c6a6a368a6cedec
김건우 <96411818+rjswjddn@users.noreply.github.com>
2025-07-21 16:49:25 +0900

    fix: import 추가

--- src/main/java/com/oronaminc/join/member/security/SecurityConfig.java
+++ src/main/java/com/oronaminc/join/member/security/SecurityConfig.java
@@ -2,6 +2,9 @@ package com.oronaminc.join.member.security;
 
 import static org.springframework.security.config.Customizer.*;
 
+import java.util.Arrays;
+import java.util.List;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
@@ -9,6 +12,9 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
 import lombok.RequiredArgsConstructor;
 
