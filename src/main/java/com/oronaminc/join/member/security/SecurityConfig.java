f181e03a7d702d8ace05d679012017c3cbfeb03f
김건우 <rjswjddn@gmail.com>
2025-07-23 17:07:47 +0900

    feat: 시큐리티에 카카오 로그인 url 추가

--- src/main/java/com/oronaminc/join/member/security/SecurityConfig.java
+++ src/main/java/com/oronaminc/join/member/security/SecurityConfig.java
@@ -27,6 +27,7 @@ public class SecurityConfig {
                 .authorizeHttpRequests(auth -> auth
                         .requestMatchers(
                                 "/api/auth/guest",
+                                "/api/auth/kakao",
                                 "/login"
                         )
                         .anonymous()
