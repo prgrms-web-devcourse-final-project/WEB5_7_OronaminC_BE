942863e3d1bae56591f7240bab16787876e0f46d
chaechae <juold0568@naver.com>
2025-07-25 12:46:15 +0900

    fix: roomId 경로변수

--- src/main/java/com/oronaminc/join/question/api/QuestionController.java
+++ src/main/java/com/oronaminc/join/question/api/QuestionController.java
@@ -10,6 +10,7 @@ import org.springframework.data.domain.Slice;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseStatus;
@@ -30,7 +31,7 @@ public class QuestionController {
         @RequestParam(required = false) Long lastEmojiCount,
         @RequestParam(defaultValue = "10") int size,
         @RequestParam Long memberId,
-        @RequestParam Long roomId
+        @PathVariable Long roomId
     ) {
         Slice<QuestionAssembleResponse> result = questionService.getQuestions(
             sort, lastId, lastEmojiCount, size, memberId, roomId
