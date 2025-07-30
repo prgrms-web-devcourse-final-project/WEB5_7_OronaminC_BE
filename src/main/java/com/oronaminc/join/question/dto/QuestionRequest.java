f5e084b2cc4f9f2dbffa00c52e2ebe93747b58e5
김건우 <96411818+rjswjddn@users.noreply.github.com>
2025-07-30 15:30:56 +0900

    test: 질문 생성 테스트 배포 (#122)
    * feat: cors 설정
    
    * 웹소켓
    
    * 웹소켓
    
    * 웹소켓
    
    * 웹소켓
    
    * 웹소켓
    
    * 웹소켓
    
    * 인터셉터, 핸들러 재추가
    
    * 핸들러 다시 추가
    
    * 핸들러 다시 주석
    
    * sockjs만 살리기
    
    * dev 와 병합
    
    * 질문 생성 변경

--- src/main/java/com/oronaminc/join/question/dto/QuestionRequest.java
+++ src/main/java/com/oronaminc/join/question/dto/QuestionRequest.java
@@ -9,7 +9,8 @@ public record QuestionRequest(
     @Schema(description = "질문 내용", example = "질문있습니다. 질문생성DTO가 맞나요?")
     @NotBlank(message = "질문 내용을 입력해주시기 바랍니다.")
     @Size(max = 500, message = "질문 내용은 최대 500자까지 입력할 수 있습니다.")
-    String content
+    String content,
+    Long memberId
 ) {
 
 }
