e9dcb2fefadc841b751c2f04021cd3db3875c39c
김건우 <rjswjddn@gmail.com>
2025-07-30 14:04:45 +0900

    sockjs만 살리기

--- src/main/java/com/oronaminc/join/websocket/config/WebSocketConfig.java
+++ src/main/java/com/oronaminc/join/websocket/config/WebSocketConfig.java
@@ -54,8 +54,8 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
                 // .setHandshakeHandler(handshakeHandler)
                 .withSockJS();
 
-        registry.addEndpoint("/ws")
-                .setAllowedOriginPatterns("*")
+        // registry.addEndpoint("/ws")
+        //         .setAllowedOriginPatterns("*")
                 // .addInterceptors(new HttpSessionHandshakeInterceptor())
                 // .setHandshakeHandler(handshakeHandler)
         ;
