1e33e4abf70032bb6499d963f3457c10a7756307
김건우 <rjswjddn@gmail.com>
2025-07-31 09:30:45 +0900

    fix: 로그인 anonymous 해제

--- src/main/java/com/oronaminc/join/member/security/SecurityConfig.java
+++ src/main/java/com/oronaminc/join/member/security/SecurityConfig.java
@@ -30,12 +30,12 @@ public class SecurityConfig {
                 .csrf(csrf -> csrf.disable())
                 .cors(cors -> cors.configurationSource(corsConfigurationSource()))
                 .authorizeHttpRequests(auth -> auth
-                        .requestMatchers(
-                                "/api/auth/guest",
-                                "/api/auth/kakao",
-                                "/login"
-                        )
-                        .anonymous()
+                        // .requestMatchers(
+                        //         "/api/auth/guest",
+                        //         "/api/auth/kakao",
+                        //         "/login"
+                        // )
+                        // .anonymous()
                         .requestMatchers(
                                 "/swagger-ui/**",
                                 "/swagger-resources/**",
